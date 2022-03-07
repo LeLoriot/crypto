891e25d66b6522cd793222132257892f03df1d46
K4nen <menadiermelodie@gmail.com>
2022-03-07 15:04:01 +0100

    Affichage de la liste des cryptos

---
+++ crypto/src/Controller/CryptoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Crypto;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CryptoController extends AbstractController
+{
+    /**
+     * @Route("/crypto", name="app_crypto")
+     */
+    public function index(): Response
+    {
+        return $this->render('crypto/index.html.twig', [
+            'controller_name' => 'CryptoController',
+        ]);
+    }
+
+    /**
+     * Lister toutes les cryptos.
+     * @Route("/lesCryptos/", name="crypto.list") * @return Response
+     */
+    public function list() : Response
+    {
+        $cryptos = $this->getDoctrine()->getRepository(Crypto::class)->findAll();
+        return $this->render('crypto/list.html.twig', [
+            'cryptos' => $cryptos, ]);
+    }
+}
+
